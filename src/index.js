724eec1f6b30fb44e2a67e9888b2df521426bc6c
shariquearshad <shariquearshad@gmail.com>
2020-12-13 19:10:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-var http = require("http");
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    if(req.url==='/welcome'){
+        res.writeHead(200, {"Content-Type": "application/json"});
 
-const httpServer = http.createServer(handleServer);
+        res.write('Welcome to Dominose!');
+        res.end();
+    }
+    else if(req.url==='/contact'){
+        var str='{"phone":18602100000,"email":"guestcaredominos@jublifood.com"}';
+        var obj=JSON.parse(str);
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.write(JSON.stringify(obj));
+        
+        res.end();
+    }
+    else{
+        res.writeHead(404, {"Content-Type": "application/json"});
 
-
-function handleServer(req, res) {
-  
-}
-
-module.exports = httpServer;
+    }
+    
+});
+console.log("listening to client req");
+server.listen(8081);
